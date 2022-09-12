4eb0e4cbede633c3912b5273d16d4d115fdbb636
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2022-09-12 12:57:16 +0300

    cqlata zad

--- ivancho/ivancho/Program.cs
+++ ivancho/ivancho/Program.cs
@@ -1,4 +1,28 @@
-﻿int i = int.Parse(Console.ReadLine());
-Console.WriteLine(i);
-string x = Console.ReadLine();
-Console.WriteLine(x + i);
+﻿double bt = int.Parse(Console.ReadLine());
+double nb = int.Parse(Console.ReadLine());
+double t;
+string g;
+double w = 0;
+double f = 0;
+double sf = 0;
+
+for (int i = 0; i < bt; i++)
+{
+    g = Console.ReadLine();
+    if (g == "W")
+    {
+        w++;
+    }
+    else if (g == "F")
+    {
+        f++;
+    }
+    else if (g == "SF")
+    {
+        sf++;
+    }
+}
+double pr =( w+f+sf )/ 100;
+Console.WriteLine(nb + (w * 2000) + (f * 1200) + (sf * 720));
+Console.WriteLine(((w * 2000) + (f * 1200) + (sf * 720)) / bt);
+Console.WriteLine(w/pr);
